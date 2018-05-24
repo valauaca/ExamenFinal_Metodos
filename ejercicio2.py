cfeb597d90008438c898eaf1dd210f242b14cf4c
MendezV <som.felipe66@gmail.com>
2018-05-24 18:13:57 -0500

    Examen Final

---
+++ ejercicio2.py
@@ -0,0 +1,10 @@
+# Ejercicio 2
+# Complete el siguiente codigo para recorrer la lista `x` e imprima
+# los numeros impares y que pare de imprimir al encontrar un numero mayor a 800
+import numpy as np
+
+x = np.int_(np.random.random(100)*1000)
+print(x)
+
+
+
---
+++ ejercicio3.py
@@ -0,0 +1,9 @@
+#Ejercicio3
+# Los arrays `u` y `v` representan dos series en funcion del tiempo `t`.
+# Grafique las dos series de datos en una misma imagen 'serie.png'
+# Calcule la covarianza entre `u` y `v` e imprima su valor.
+
+import numpy as np
+t = np.array([0.,0.1,0.2,0.3,0.4,0.5,0.6, 0.8, 0.9])
+u = np.array([12.,45.,6.,78.,34.,22.,-10.,31.,-27.])
+v = np.array([3.,11.,1.3,37.,11.,6.,-23.,7.,7.])
